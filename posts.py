418549433179d7f4fd9ec21ed1f0fdd717273038
chzesa <chzesa@gmail.com>
2021-09-05 17:03:55 +0300

    Add recursive query for fetching all replies

--- posts.py
+++ posts.py
@@ -115,11 +115,23 @@ def thread_id_from_post_id(id):
     return result.fetchone()[0]
 
 def get_replies(post_id):
-    sql = """SELECT DISTINCT ON (content.post_id) content, username, posts.id, posts.status, posts.created FROM posts
-        LEFT JOIN content ON content.post_id = posts.id
-        LEFT JOIN users ON users.id = posts.user_id
-        WHERE posts.parent_id = :post_id
-        ORDER BY content.post_id, content.edited DESC"""
+    sql = """
+        SELECT * FROM (
+            WITH RECURSIVE T1
+                (id, user_id, created, status, parent_id, level)
+            AS (
+                SELECT id, user_id, parent_id, created, status, 1 FROM posts WHERE parent_id=:post_id
+                UNION ALL
+                SELECT posts.id, posts.user_id, posts.parent_id, posts.created, posts.status, T1.level + 1 FROM posts, T1
+                WHERE posts.parent_id = T1.id
+            )
+            SELECT DISTINCT ON (content.post_id) content, username, T1.id, T1.parent_id, T1.status, T1.created, level FROM T1
+            LEFT JOIN content ON content.post_id = T1.id
+            LEFT JOIN users ON users.id = T1.user_id
+            ORDER BY content.post_id, content.edited DESC
+        )  AS T2
+        ORDER BY T2.parent_id, T2.created
+        """
     result = db.session.execute(sql, {"post_id": post_id})
     rows = result.fetchall()
     return rows
